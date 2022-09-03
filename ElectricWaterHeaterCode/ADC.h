a3127aa223a3bf86cfece1c613eb9b2d5fd96267
unknown <abdulrahmanmuhammad2811@gmail.com>
2022-09-03 13:22:25 +0200

    ElectricWaterHeaterProject

---
+++ ElectricWaterHeaterCode/ADC.h
@@ -0,0 +1,22 @@
+
+
+#ifndef ADC_H
+#define	ADC_H
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+
+#include <avr/io.h>
+#include "std_macros.h"
+
+
+void ADC_init(void);
+uint16_t ADC_read(void);
+
+
+#endif /* ADC_H_ */
+
+#endif	/* ADC_H */
+
